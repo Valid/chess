7e6b1ce02ff3afb1908febc7d25c1bed55cae932
Jon Hughes <github@validatorian.com>
2018-04-03 23:42:22 -0700

    Add base logic and functionality

--- src/components/ChessBoard/index.js
+++ src/components/ChessBoard/index.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import isEqual from 'lodash.isequal';
+import { shape, arrayOf, oneOfType, string, number, func } from 'prop-types';
 import Tile from '../Tile';
 
-const ChessBoard = () => {
-  // The standard chessboard is 8x8, so we start by creating an blank array with 64 tiles
-  const BOARD_SIZE = 8;
-  const gameScaffolding = [...Array(BOARD_SIZE * BOARD_SIZE)];
-  return (
-    <div className="chessboard">
-      {gameScaffolding.map((tile, index) => {
-        // Store the [x, y] coords for each tile
-        const coords = [Math.floor(index / BOARD_SIZE), index % BOARD_SIZE];
-        return <Tile coords={coords} key={coords} />;
-      })}
-    </div>
-  );
+class ChessBoard extends Component {
+  render() {
+    const { active, pieces, selectTile } = this.props;
+    const BOARD_SIZE = 8;
+    // The standard chessboard is 8 x 8, so we start by creating an blank array with 64 tiles
+    const gameScaffolding = [...Array(BOARD_SIZE * BOARD_SIZE)];
+    return (
+      <div className="chessboard">
+        {gameScaffolding.map((tile, index) => {
+          // Store the [x, y] coords for each tile
+          const coords = [Math.floor(index / BOARD_SIZE), index % BOARD_SIZE];
+          // Cross-references passed piece locations to determine where to place them
+          const piece = Object.entries(pieces).find((pieceCoords) => isEqual(pieceCoords[1], coords));
+          // Check if piece is active (for custom styling)
+          const isActive = piece && active && isEqual(piece, active);
+          return <Tile coords={coords} key={coords} piece={piece} active={isActive} selectTile={selectTile} />;
+        })}
+      </div>
+    );
+  }
+}
+
+ChessBoard.defaultProps = {
+  active: null,
+  pieces: {},
+};
+
+ChessBoard.propTypes = {
+  active: arrayOf(oneOfType([string.isRequired, arrayOf(number.isRequired)])),
+  pieces: shape({}),
+  selectTile: func.isRequired,
 };
 
 export default ChessBoard;
--- src/containers/ChessBoardContainer.js
+++ src/containers/ChessBoardContainer.js
@@ -1,10 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react';
+import isEqual from 'lodash.isequal';
 import ChessBoard from '../components/ChessBoard';
+import { isValidMove } from '../components/Piece';
 
 import './ChessBoard.css';
 
-const ChessBoardContainer = () => {
-  return <ChessBoard />;
-};
+class ChessBoardContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    // Here you can modify the start position for each piece
+    this.state = {
+      pieces: {
+        knight: [0, 1],
+        bishop: [0, 5],
+      },
+      active: null,
+    };
+
+    this.selectTile = this.selectTile.bind(this);
+  }
+  selectTile(piece, coords) {
+    const { active, pieces } = this.state;
+
+    // If same piece, deselect
+    if (isEqual(piece, active)) {
+      this.setState({ active: null });
+      return;
+    }
+    if (active) {
+      // Check if move is valid
+      if (!isValidMove(active, coords)) {
+        return;
+      }
+
+      // Check if space is occupied by another piece
+      if (Object.entries(pieces).find((pieceCoords) => isEqual(pieceCoords[1], coords))) {
+        return;
+      }
+
+      // All checks done, move that piece!
+      this.setState((prevState) => ({
+        pieces: {
+          // This carries forward existing pieces
+          ...prevState.pieces,
+          // Then updates the current piece with new coords
+          [active[0]]: coords,
+        },
+      }));
+    }
+
+    // If the user clicked on a new piece, mark it as active
+    this.setState({
+      active: piece,
+    });
+  }
+  render() {
+    return <ChessBoard {...this.state} selectTile={this.selectTile} />;
+  }
+}
 
 export default ChessBoardContainer;
